4a133856b7e7d769e1bda3e9ebde8782a7b156cd
Tuan Tran <mptnt1988@gmail.com>
2017-06-27 22:55:33 +0700

    Add dotEmacs module for Octave

---
+++ dotEmacs/tnt_octave.el
@@ -0,0 +1,17 @@
+;; Introduction:
+;;   This module is used for setup and configure Emacs Octave Support
+;; Note:
+;;   Add path to this file to 'load-path
+;;     (add-to-list 'load-path "/path/to/this/file")
+;;   then
+;;     (load "tnt_octave.el")
+
+(setq auto-mode-alist
+      (cons '("\\.m$" . octave-mode) auto-mode-alist))
+
+(add-hook 'octave-mode-hook
+          (lambda ()
+            (abbrev-mode 1)
+            (auto-fill-mode 1)
+            (if (eq window-system 'x)
+                (font-lock-mode 1))))
