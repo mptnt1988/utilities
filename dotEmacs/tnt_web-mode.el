914871acab2237765731010a12124bb1aa79f8e7
Tuan Tran <mptnt1988@gmail.com>
2017-07-23 11:03:58 +0700

    Add php-mode to dotEmacs module for web prog mode

--- dotEmacs/tnt_web-mode.el
+++ dotEmacs/tnt_web-mode.el
@@ -1,7 +1,17 @@
 ;; Note:
-;;   Install web-mode
 ;;   Add path to this file to 'load-path
 ;;     (add-to-list 'load-path "/path/to/this/file")
 ;;   then
 ;;     (load "tnt_web-mode.el")
+
+(defvar myPkgs_web
+  '(web-mode
+    php-mode
+    ))
+
+(mapc #'(lambda (package)
+          (unless (package-installed-p package)
+            (package-install package)))
+      myPkgs_web)
+
 (require 'web-mode)
