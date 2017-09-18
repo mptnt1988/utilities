9c4065ce9bbf5d1221550d51aca227d63c030639
Tuan Tran <mptnt1988@gmail.com>
2017-09-18 04:02:15 -0700

    Update dotEmacs tnt_abbrev.el

--- dotEmacs/tnt_abbrev.el
+++ dotEmacs/tnt_abbrev.el
@@ -15,11 +15,58 @@
 ;; to turn on abbrev-mode globally
 (setq-default abbrev-mode t)
 
-;; setting abbreviation
+;; ;; HOOK FUNCTIONS
+(defun dont-insert-expansion-char () t)
+(put 'dont-insert-expansion-char 'no-self-insert t)
+
+(defun add-erl-module-name ()
+  (backward-char 2)
+  (insert (file-name-sans-extension (buffer-name)))
+  (move-to-new-line)
+  t)
+(put 'add-erl-module-name 'no-self-insert t)
+
+(defun move-to-new-line ()
+  (end-of-line)
+  (newline-and-indent)
+  )
+(put 'move-to-new-line 'no-self-insert t)
+
+(require 'cl)
+(defvar my-abbrev-tables nil)
+(defun my-abbrev-hook ()
+  (let ((def (assoc (symbol-name last-abbrev) my-abbrev-tables)))
+    (when def
+      (execute-kbd-macro (cdr def)))
+    t))
+(put 'my-abbrev-hook 'no-self-insert t)
+(defmacro declare-abbrevs (table abbrevs)
+  (if (consp table)
+      `(progn ,@(loop for tab in table
+                      collect `(declare-abbrevs ,tab ,abbrevs)))
+    `(progn
+       ,@(loop for abbr in abbrevs
+               do (when (third abbr)
+                    (push (cons (first abbr) (read-kbd-macro (third abbr)))
+                          my-abbrev-tables))
+               collect `(define-abbrev ,table
+                          ,(first abbr) ,(second abbr) ,(and (third abbr)
+                                                             ''my-abbrev-hook))))))
+(put 'declare-abbrevs 'lisp-indent-function 2)
+
+;; SETTING ABBREVIATION
 (clear-abbrev-table global-abbrev-table)
+
 (define-abbrev-table 'global-abbrev-table
   '(
     ("mpube" "#!/usr/bin/env")
-    ("mperlio" "io:format(\"MPTrace: ~p~n\", [[{?MODULE, ?LINE}]]),")
+    ("mperlm" "-module()." add-erl-module-name)
+    ("mperlc" "-compile(export_all)." move-to-new-line)
     )
+  )
+
+(declare-abbrevs (global-abbrev-table)
+    (
+     ("mperlio" "io:format(\": ~p~n\", [[{?MODULE, ?LINE}]])," "C-u 3 1 C-b")
+     )
   )
