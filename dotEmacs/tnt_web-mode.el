34fa5f3b3a4fef9934bbe923019d87674090e568
Tuan Tran <mptnt1988@gmail.com>
2017-09-24 13:08:14 +0700

    Temp add modes in dotEmacs tnt_web-mode.el

--- dotEmacs/tnt_web-mode.el
+++ dotEmacs/tnt_web-mode.el
@@ -5,7 +5,9 @@
 ;;     (load "tnt_web-mode.el")
 
 (defvar myPkgs_web
-  '(web-mode
+  '(
+    smartparens
+    web-mode
     php-mode
     ))
 
@@ -14,4 +16,48 @@
             (package-install package)))
       myPkgs_web)
 
+;;------------------
+;; SMARTPARENS MODE
+;;------------------
+(require 'smartparens)
+
+;;------------------
+;; WEB MODE
+;;------------------
 (require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.blade\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("/\\(views\\|html\\|templates\\)/.*\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+(setq web-mode-engines-alist
+      '(("php"    . "\\.phtml\\'")
+        ("blade"  . "\\.blade\\."))
+      )
+
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (local-set-key (kbd "RET") 'newline-and-indent)
+  )
+(add-hook 'web-mode-hook 'my-web-mode-hook)
+
+(defun sp-web-mode-is-code-context (id action context)
+  (and (eq action 'insert)
+       (not (or (get-text-property (point) 'part-side)
+                (get-text-property (point) 'block-side))))
+  )
+(sp-local-pair 'web-mode "<" nil :when '(sp-web-mode-is-code-context))
+
+;;------------------
+;; PHP MODE
+;;------------------
+(require 'php-mode)
+(add-to-list 'auto-mode-alist '("\\.php\\'" . php-mode))
