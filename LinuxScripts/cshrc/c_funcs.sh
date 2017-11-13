7ca0667c17e648c9f06b075ba01d5ace2ff21c59
Tuan Tran <mptnt1988@gmail.com>
2017-11-12 21:44:25 -0800

    Add CSH key binding for CLI backward search

--- LinuxScripts/cshrc/c_funcs.sh
+++ LinuxScripts/cshrc/c_funcs.sh
@@ -7,6 +7,9 @@
 ## The call the functions which are aliases
 ##------------------------------------------------------------------------------
 
+## Key bindings
+bindkey ^r i-search-back
+
 ## Set color prompt
 ## Note: Changing alias precmd and var mp_scriptPath will affect this function
 alias mp_set_prompt "source $mp_scriptPath/mp_set_prompt"
