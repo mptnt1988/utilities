848cdcb01c56767895a99f75ea01e66e0dbb8512
Tuan N. Tran <nhattuan.tran@ved.com.vn>
2018-11-16 17:37:19 +0700

    Add dotEmacs go-mode tab width

--- dotEmacs/tnt_go.el
+++ dotEmacs/tnt_go.el
@@ -16,6 +16,13 @@
 
 (tntLib_install-myPkgs)
 
+(defun my-go-mode-hook ()
+  "Define custom hook for go-mode."
+  (setq tab-width 4)  ; tab width is 4 spaces
+  (setq indent-tabs-mode nil)  ; spaces instead of tab
+  )
+(add-hook 'go-mode-hook 'my-go-mode-hook)
+
 (require 'go-mode)
 
 (provide 'tnt_go)
